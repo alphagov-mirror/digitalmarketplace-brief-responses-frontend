a432d3f215c6e82df80985c08ce61eb72f7bfd9c
BRETT KYLE <brett.kyle@digital.cabinet-office.gov.uk>
2021-01-11 16:43:37 +0000

    Adjust app tests
    Does not yet include fixes for the niceToHave question tests, since this will be handled by another PR.

--- tests/app/main/test_briefs.py
+++ tests/app/main/test_briefs.py
@@ -276,7 +276,7 @@ class TestBriefClarificationQuestions(BaseApplicationTest):
 
         res = self.client.post('/suppliers/opportunities/1/ask-a-question')
         xpath = html.fromstring(res.get_data(as_text=True)).xpath
-        advice = xpath("//span[contains(@class, 'dm-question-advice')]/p/text()")[0]
+        advice = xpath("//div[contains(@class, 'dm-question-advice')]/p/text()")[0]
 
         assert "Tuesday 29 March 2016" in advice
 
@@ -290,7 +290,7 @@ class TestBriefClarificationQuestions(BaseApplicationTest):
         res = self.client.post('/suppliers/opportunities/1/ask-a-question')
         xpath = html.fromstring(res.get_data(as_text=True)).xpath
 
-        guidance_url = xpath("//span[contains(@class, 'govuk-hint')]//a/@href")[0]
+        guidance_url = xpath("//div[contains(@class, 'govuk-hint')]//a/@href")[0]
 
         assert guidance_url.startswith("https://www.gov.uk/guidance")
 
